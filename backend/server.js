b70f88d3a692c58ca9ea5ef0f99490e3a2df8765
Om Suthar <ompsuthar9000@gmail.com>
2025-03-06 16:18:03 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
 }));
